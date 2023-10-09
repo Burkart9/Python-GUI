d94456c4e5efc8bd5ffe936ffe5f9f1d14fd48ad
Burkart <2753064303@qq.com>
2023-10-10 00:18:20 +0800

    update

--- ttkgui.py
+++ ttkgui.py
@@ -3,7 +3,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
 # Window
-root = ttk.Window(title="My ttk Window",themename="darkly",size=(500,400),iconphoto="Graphics/notebook2.png")
+root = ttk.Window(title="My ttk Window", themename="darkly", size=(500, 400), iconphoto="Graphics/notebook2.png")
 root.position_center()
 
 # Functions
@@ -12,18 +12,9 @@ def test():
 
 # Widgets
 b1 = ttk.Button(root, text="Button 1", command=test, bootstyle=SUCCESS)
-b1.pack(side=LEFT, padx=5, pady=10)
+b1.pack(side=LEFT, padx=5, pady=10, expand=True, fill=X)
 b2 = ttk.Button(root, text="Button 2", bootstyle=(INFO, OUTLINE))
-b2.pack(side=LEFT, padx=5, pady=10)
-
-
-
-
-
-
-
-
-
+b2.pack(side=LEFT, padx=5, pady=10, expand=True, fill=X)
 
 # Mainloop
 root.mainloop()
